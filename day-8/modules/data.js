306a499a6e0e90e3db5c01cf62600c3789662d7a
Thoriq Nur Faizal <thoriiiq@gmail.com>
2020-08-31 15:25:49 +0700

    Adding materi oop, prototype, modules,

---
+++ day-8/modules/data.js
@@ -0,0 +1,32 @@
+export let fruit = [
+  {
+    nama: "Pisang",
+    latin: "Musa acuminata",
+    warna: "Kuning",
+  },
+  {
+    nama: "Jambu",
+    latin: "Psidium guajava",
+    warna: "Hijau",
+  },
+  {
+    nama: "Apel",
+    latin: "Malus domestica",
+    warna: "Merah",
+  },
+  {
+    nama: "Durian",
+    latin: "Durio zibethinus",
+    warna: "Kuning",
+  },
+  {
+    nama: "Mangga",
+    latin: "Mangifera indica",
+    warna: "Hijau-Oranye",
+  },
+  {
+    nama: "Semangka",
+    latin: "Citrullus lanatus",
+    warna: "Hijau-merah",
+  },
+];
---
+++ day-8/modules/greeting.js
@@ -0,0 +1,13 @@
+// function katakanHalo(user) {
+//   alert(`Hello, ${user}`);
+// }
+
+// function katakanTerimakasih(user) {
+//   console.log(`Terimakasih ${user}`);
+// }
+
+// // export { katakanHalo as sayHello, katakanTerimakasih as sayThank };
+
+export default function katakanHalo(user) {
+  alert(`Hello, ${user}`);
+}
