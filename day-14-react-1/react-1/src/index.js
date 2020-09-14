9db21f483ad06a1b87c3a5f9d5bf47dfe9ff1abe
Thoriq Nur Faizal <thoriiiq@gmail.com>
2020-09-14 11:54:14 +0700

    Adding materi react

---
+++ day-14-react-1/react-1/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import { Aplikasi } from "./App";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Aplikasi />
+  </React.StrictMode>,
+  document.getElementById("roots")
+);
