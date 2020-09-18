7a93271c7d55b1b80d6d171a8529c5d2b08d649c
Thoriq Nur Faizal <thoriiiq@gmail.com>
2020-09-18 10:01:03 +0700

    Adding materi useEffect

---
+++ day-16-react-fetching/react-fetching/src/App.js
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState("posts");
+  const [render, setRender] = useState([]);
+  const [countNum, setCountNum] = useState(0);
+
+  const decrement = () => {
+    setCountNum(countNum - 1);
+  };
+  // rules 1 = setiap react render ulang useEffect dipanggil / state berubah
+  // dengan ditambahkan [] setelah fetch berarti kita hanya watch / subscribe ke perubahan data tersebut saja
+  useEffect(() => {
+    console.log("mounting");
+    fetch(`https://jsonplaceholder.typicode.com/${data}`)
+      .then((response) => response.json())
+      .then((result) => setRender(result));
+  }, [data]);
+
+  // useEffect(() => {
+  //   effect
+  // }, [input])
+  return (
+    <div className="App">
+      <h1>React Fetching data</h1>
+      <div>
+        <button onClick={() => setData("posts")}>Posts</button>
+        <button onClick={() => setData("users")}>Users</button>
+        <button onClick={() => setData("comments")}>Comments</button>
+      </div>
+      <div>{data}</div>
+      <div>
+        {render.map((item, id) => (
+          <div key={id}>
+            <p>{JSON.stringify(item)}</p>
+          </div>
+        ))}
+      </div>
+      <div>
+        <h1 style={{ color: "red" }}>Counter Function</h1>
+        <button onClick={decrement}>-</button>
+        <h1 style={{ display: "inline" }}>{countNum}</h1>
+        <button>+</button>
+        <hr />
+      </div>
+    </div>
+  );
+}
+
+export default App;
