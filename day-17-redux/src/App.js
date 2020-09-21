a86250310d4c5263dc8642452511c6a847b2ab52
Thoriq Nur Faizal <thoriiiq@gmail.com>
2020-09-21 10:36:42 +0700

    Adding store to redux

---
+++ day-17-redux/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Counter from "./page/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
