852f1d90fa8794d8f9a98663e38ad0bb9cc71150
Thoriq Nur Faizal <thoriiiq@gmail.com>
2020-10-05 14:17:50 +0700

    mongoose backend

--- day-23-node-mongo/index.js
+++ day-23-node-mongo/index.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 const { PORT, MONGODB_URI_LIVE } = require("./config");
 const localPort = PORT || 9000;
 
 const { dbMongo } = require("./config");
+const studentRouter = require("./routes/StudentController");
 
 console.log("port", PORT);
 console.log("MONGODB_URI_LIVE", MONGODB_URI_LIVE);
 // console.log("db", dbMongo);
 
+app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("halo");
 });
+
+app.use(studentRouter);
 
 if (dbMongo) {
   app.listen(localPort, () => {
