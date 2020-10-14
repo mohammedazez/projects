d13d28c27bcf78c4585be627d0ec314665adfd35
Sarah Julia Gushef <sarahgushef@gmail.com>
2020-10-14 11:08:32 +0700

    Enhance login feature

--- day-25-jwt/express-mongoose-jwt/routes/student/index.js
+++ day-25-jwt/express-mongoose-jwt/routes/student/index.js
@@ -1,30 +1,28 @@
-var express = require("express");
+var express = require('express');
 var router = express.Router();
 
 const {
-  getAllStudent,
-  getStudentById,
-  postStudent,
-  getStudentInClass,
-  loginStudent
-} = require("./controller");
-const {auth} = require("../../helper/auth")
+    getAllStudent,
+    getStudentById,
+    postStudent,
+    getStudentInClass,
+    loginStudent,
+} = require('./controller');
+const { auth } = require('../../helper/auth');
 
-router.get("/me", auth, (req, res) => {
-  res.json({
-    message: "Hallo user",
-    user: req.body
-  })
-})
+router.get('/me', auth, (req, res) => {
+    res.json({
+        message: 'Hallo user',
+        user: req.body,
+    });
+});
 
 /* GET users listing. */
-router.get("/", getAllStudent);
-router.get("/:id", getStudentById);
-router.get("/class/:id", getStudentInClass);
-router.post("/", postStudent);
+router.get('/', getAllStudent);
+router.get('/:id', getStudentById);
+router.get('/class/:id', getStudentInClass);
+router.post('/', postStudent);
 
-router.post("/login", loginStudent);
-
-
+router.post('/login', loginStudent);
 
 module.exports = router;
