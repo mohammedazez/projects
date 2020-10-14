6e91a6acea912bff7fbf07da03f077c7c68a7ba1
Sarah Julia Gushef <sarahgushef@gmail.com>
2020-10-14 11:31:34 +0700

    Update env.example

--- day-26-jwt-react/src/App.js
+++ day-26-jwt-react/src/App.js
@@ -1,30 +1,33 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Register from "./pages/Register";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+
+import './App.css';
+import PrivateRoute from './helpers/PrivateRoute';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Redux Login</h1>
+    return (
+        <div className='App'>
+            <h1>Redux Login</h1>
 
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/register">
-            <Register />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+            <Router>
+                <Switch>
+                    <PrivateRoute exact path='/'>
+                        <Home />
+                    </PrivateRoute>
+                    <Route path='/login'>
+                        <Login />
+                    </Route>
+                    <Route path='/register'>
+                        <Register />
+                    </Route>
+                </Switch>
+            </Router>
+        </div>
+    );
 }
 
 export default App;
